397e5904e338fb4cf4fdef545f5b1da700439dca
VPetrashchu <bridok1990@gmail.com>
2022-04-20 22:09:12 +0300

    add classes

--- class/ex2.py
+++ class/ex2.py
@@ -1,19 +1,27 @@
 class Hause():
     
-    def __init__(self, wall, lacation, squaled, plane):
+    def __init__(self, wall, lacation, plane):
         self.wall = wall
         self.lacation = lacation
-        self.squaled = squaled
         self.plane = plane
-
-class Pool(Hause):
     
 class Building(Hause):
     
-    def __init__(self, floor):
+    def __init__(self, floor, wall, lacation, plane):
+        super.__init__(self, wall, lacation, plane)
         self.floor = floor
+        
 
 class Cottage(Hause):
+    def __init__(self, length, width, depth, wall, lacation, plane):
+        super.__init__(self, wall, lacation, plane)
+        
+        self.pool = Pool(length, width, depth)
+
+class Pool(): 
+    
     def __init__(self, length, width, depth):
-        super.__init__(self, length, width, depth)
-        self.pool = Pool(self.length, self.width, self.depth)
+        self.length = length
+        self.width = width
+        self.depth = depth
+
