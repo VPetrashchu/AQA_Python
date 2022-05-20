86fc5b0e3ecec2b53ff0586c3ebf80ec0cd1959d
VPetrashchu <bridok1990@gmail.com>
2022-05-20 12:54:17 +0300

    add set join

---
+++ restudy/set_join.py
@@ -0,0 +1,7 @@
+#union() method returns a new set with all items from both sets
+
+set1 = {'apple', 'orange', 'kiwi'}
+set2 = {'android', 'apple', 'microsoft'}
+
+set_join = set1.union(set2)
+print(set_join)
